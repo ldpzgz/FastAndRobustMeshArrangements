f1dafa4c2d5e8bdb0e9e2cf43031bfbec71a0ffd
dongping liu <460054202@qq.com>
2023-10-31 10:44:24 +0800

    Create meshArrangement.h

---
+++ meshArrangement.h
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<bitset>
+#include<string>
+/*
+* 三角化有相交的三角形
+*/
+void solveIntersectTriangles(std::vector<double>& in_coords,
+    std::vector<unsigned int>& in_tris,
+    std::vector<double>& out_coords,
+    std::vector<unsigned int>& out_tris);
+/*
+* 三角化有相交的三角形，label标记三角形属于哪个mesh，
+* out_labels标记输出的三角形属于哪个mesh
+*/
+void solveIntersectTriangles(std::vector<double>& in_coords,
+    std::vector<unsigned int>& in_tris,
+    std::vector< std::bitset<32>>& in_labels,
+    std::vector<double>& out_coords,
+    std::vector<unsigned int>& out_tris,
+    std::vector< std::bitset<32>>& out_labels);
